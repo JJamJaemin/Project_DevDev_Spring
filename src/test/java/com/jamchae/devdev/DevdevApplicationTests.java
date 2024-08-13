710c4ea279e04fd9f6f44370566f93db891e0a9f
Bae Jae Min <128075457+JJamJaemin@users.noreply.github.com>
2024-08-13 14:07:27 +0900

    Initial commit

---
+++ src/test/java/com/jamchae/devdev/DevdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamchae.devdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
